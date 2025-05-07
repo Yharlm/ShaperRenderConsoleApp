f4cb837b0f47ce1dc1b7efa584adf8ec5920c318
Yharlm <lambrins22@gmail.com>
2025-05-07 10:33:51 +0300

    drawing lines, creating shapes from points, next up try to make 3d render

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -11,7 +11,8 @@ namespace ConsoleApp8
             public int y;
         }
 
-        class point {
+        class point
+        {
             public string name;
             public Cordinates pos;
             public point(string name, int x, int y)
@@ -25,53 +26,73 @@ namespace ConsoleApp8
 
         class Shape
         {
-            List<point> points = new List<point>();
-
+            public List<point> points = new List<point>();
+            public void AddPoint(string name, int x, int y)
+            {
+                points.Add(new point(name, x, y));
+            }
         }
-        
+
         static void Main(string[] args)
         {
             int size = 50;
             int[,] grid = new int[size, size];
 
-            Shape shape = new Shape();
 
 
-            for (int i = 0; i < size; i++)
+            int x = 0;
+            int y = 0;
+            while (true)
             {
-                for (int j = 0; j < size; j++)
+                
+                Shape shape = new Shape();
+                shape.AddPoint("A", 10, 10);
+                shape.AddPoint("B", 20+x, 10+x);
+                shape.AddPoint("C", 20-y, 20+y);
+                shape.AddPoint("D", 10, 20);
+
+                Console.ReadKey(); x++;y++; Console.Clear();
+                for (int i = 0; i < size; i++)
                 {
-                    grid[i, j] = 0;
+                    for (int j = 0; j < size; j++)
+                    {
+                        grid[i, j] = 0;
+                    }
+                }
+
+                for (int i = 1; i < shape.points.Count; i++)
+                {
+                    DrawLine(shape.points[i], shape.points[i - 1], grid);
+                }
+                DrawLine(shape.points[0], shape.points[shape.points.Count - 1], grid);
+
+
+
+
+
+                for (int i = 0; i < size - 1; i++)
+                {
+                    for (int j = 0; j < size - 1; j++)
+                    {
+                        if (grid[i, j] == 0)
+                        {
+                            Console.Write("  ");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.Write("[]");
+                            Console.ResetColor();
+                        }
+
+                    }
+                    Console.WriteLine();
                 }
             }
-            DrawLine(A, B, grid);
-            
-
-
-
-            for (int i = 0; i < size - 1; i++)
-            {
-                for (int j = 0; j < size - 1; j++)
-                {
-                    if (grid[i, j] == 0)
-                    {
-                        Console.Write("  ");
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.Write("[]");
-                        Console.ResetColor();
-                    }
-
-                }
-                Console.WriteLine();
-            }
-
 
         }
 
-        static void DrawLine(point a,point b, int[,] grid)
+        static void DrawLine(point a, point b, int[,] grid)
         {
             Cordinates A = a.pos;
             Cordinates B = b.pos;
