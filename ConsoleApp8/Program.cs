2fe31c21de1c89c265ea2c76bd2b8700d286a631
lambrin <133050108+Yharlm@users.noreply.github.com>
2025-05-08 13:20:16 +0300

    Update Program.cs

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace ConsoleApp8
+namespace ConsoleApp15
 {
     internal class Program
     {
+        static void WriteAt(string text, int x, int y)
+        {
+            try
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(text);
+            }
+            catch { }
+
+        }
         class Cordinates
         {
             public int x;
@@ -31,63 +38,122 @@ namespace ConsoleApp8
             {
                 points.Add(new point(name, x, y));
             }
+            public void SquarePoint()
+            {
+                this.AddPoint("A", 10, 10);
+                this.AddPoint("B", 20, 10);
+                this.AddPoint("C", 20, 20);
+                this.AddPoint("D", 10, 20);
+            }
         }
 
         static void Main(string[] args)
         {
             int size = 50;
             int[,] grid = new int[size, size];
+            List<Shape> shapes = new List<Shape>();
+            Shape Sqaure = new Shape();
+            Sqaure.AddPoint("A", 10, 10);
+            Sqaure.AddPoint("B", 20, 10);
+            Sqaure.AddPoint("C", 20, 20);
+            Sqaure.AddPoint("D", 10, 20);
 
-
-
-            int x = 0;
-            int y = 0;
+            string input = "";
+            int index = 1;
+            int shape_index = 0;
             while (true)
             {
                 
-                Shape shape = new Shape();
-                shape.AddPoint("A", 10, 10);
-                shape.AddPoint("B", 20+x, 10+x);
-                shape.AddPoint("C", 20-y, 20+y);
-                shape.AddPoint("D", 10, 20);
 
-                Console.ReadKey(); x++;y++; Console.Clear();
-                for (int i = 0; i < size; i++)
+                foreach(var shape1 in shapes)
                 {
-                    for (int j = 0; j < size; j++)
+                    for (int i = 1; i < shape1.points.Count; i++)
                     {
-                        grid[i, j] = 0;
+                        DrawLine(shape1.points[i], shape1.points[i - 1], grid);
                     }
+                    DrawLine(shape1.points[0], shape1.points[shape1.points.Count - 1], grid);
                 }
-
-                for (int i = 1; i < shape.points.Count; i++)
-                {
-                    DrawLine(shape.points[i], shape.points[i - 1], grid);
-                }
-                DrawLine(shape.points[0], shape.points[shape.points.Count - 1], grid);
+                
 
 
 
-
-
+               
                 for (int i = 0; i < size - 1; i++)
                 {
                     for (int j = 0; j < size - 1; j++)
                     {
                         if (grid[i, j] == 0)
                         {
-                            Console.Write("  ");
+                            WriteAt("  ", j * 2, i);
                         }
+
                         else
                         {
                             Console.ForegroundColor = ConsoleColor.DarkRed;
-                            Console.Write("[]");
+                            WriteAt("[]", j * 2, i);
                             Console.ResetColor();
                         }
+                        
 
                     }
-                    Console.WriteLine();
+
                 }
+                Shape shape = new Shape();
+                if (shapes.Count > 0)
+                {
+                    shape = shapes[shape_index];
+                    Console.ForegroundColor = ConsoleColor.Green;
+                WriteAt("[]", shape.points[index].pos.x * 2, shape.points[index].pos.y);
+                Console.ResetColor();
+                }
+                
+                input = Console.ReadKey().Key.ToString();
+
+
+                if (input == "S")
+                {
+                    shape.points[index].pos.y++;
+                }
+                if (input == "W")
+                {
+                    shape.points[index].pos.y--;
+                }
+                if (input == "A")
+                {
+                    shape.points[index].pos.x--;
+                }
+                if (input == "D")
+                {
+                    shape.points[index].pos.x++;
+                }
+                if (input == "D1")
+                {
+                    index++;
+                    if (shape.points.Count == index)
+                    {
+                        index = 0;
+                    }
+                }
+                if (input == "D2")
+                {
+                    shape_index++;
+                    if (shape.points.Count == shape_index)
+                    {
+                        shape_index = 0;
+                    }
+                }
+                if (input == "E")
+                {
+                    shapes.Add(Sqaure);
+                }
+                for (int i = 0; i < size - 1; i++)
+                {
+                    for (int j = 0; j < size - 1; j++)
+                    {
+                        grid[i, j] = 0;
+                    }
+                }
+
             }
 
         }
